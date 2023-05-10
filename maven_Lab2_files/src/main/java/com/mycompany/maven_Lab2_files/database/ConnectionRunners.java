2297740e9678a4c0c6f100c86ba1ae19f899b7e5
amino-acid02 <raminaz2002@mail.ru>
2023-05-10 15:12:50 +0300

    commit another connection

--- maven_Lab2_files/src/main/java/com/mycompany/maven_Lab2_files/database/ConnectionRunners.java
+++ maven_Lab2_files/src/main/java/com/mycompany/maven_Lab2_files/database/ConnectionRunners.java
@@ -3,9 +3,9 @@ package com.mycompany.maven_Lab2_files.database;
 public abstract class ConnectionRunners {
     
     public SQL_Preparator preparator;
-    protected String url = "jdbc:postgresql://localhost:5432/postgres";
-    protected  String user = "postgres";
-    protected  String password = "1234";
+    protected String url = "jdbc:postgresql://dpg-chdoc867avj0djiu76l0-a.frankfurt-postgres.render.com:5432/lab3_kt3v";
+    protected  String user = "amina";
+    protected  String password = "aTBtkIXuxOzOyNWf6QuWBR0kyDfTEdZo";
    
     public void run()
     {
